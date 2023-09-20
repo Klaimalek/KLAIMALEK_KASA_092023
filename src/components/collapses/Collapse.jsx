0bdccabd2d0c00f16f7f3afba854df0d92d8b696
Malek <absimalekmootez@gmail.com>
2023-09-20 13:36:40 +0200

    utilisation de useState pour gérer l'affichage du conteni des collapse

--- src/components/collapses/Collapse.jsx
+++ src/components/collapses/Collapse.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useState } from 'react';
+import arrow from '../../assets/arrow.svg'
+import './collapse.css';
 
-function Collapse() {
+function Collapse({ title, content }) {
+  const [toggle, setToggle] = useState('none');
+
+  const toggleState = () => {
+    if (toggle === 'none') {
+      setToggle('block');
+    }
+    else{
+      setToggle('none');
+    }
+  };
+  console.log(toggle);
   return (
-    <div>Collapse</div>
-  )
+    <div className="container-collaspe">
+      <div className="collapse-content-header">
+        <p className="collapse-title">{`${title}`} </p>
+
+        <div onClick={toggleState}  className='collapse-arrow'>
+          {toggle ==='none' ?
+          (<img src={arrow} alt="Arrow down"/>) :
+          (<img src={arrow} style={{transform: "rotate(180deg)"}} alt="Arrow up" />)}
+        </div>
+      </div>
+
+      <div className="collapse-content " style={{display: toggle}}>{`${content}`}</div>
+    </div>
+  );
 }
 
-export default Collapse
+export default Collapse;
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import Navbar from '../../components/navBar/NavBar';
 import Banner from '../../components/banner/Banner';
 import bannerApropos from '../../assets/bannerApropos.png';
+import Collapse from './../../components/collapses/Collapse';
 const About = () => {
   return (
     <div className="container-about">
       <div className="navbar-page-about">
         <Navbar />
         <Banner pictureHome={bannerApropos} messageHome="" />
+       <Collapse title = {"Fiabilité"} content={"jjjjjjjjjjjjjjjjjjjj"}/> 
+      
       </div>
     </div>
   );
