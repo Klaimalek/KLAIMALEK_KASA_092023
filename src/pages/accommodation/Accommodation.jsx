0ee9ec5625b2e014ea5aaccbe2eab6334f4ebb59
Malek <absimalekmootez@gmail.com>
2023-10-20 20:23:32 +0200

    afficher la page erreur apreès la modification d'id

--- src/pages/accommodation/Accommodation.jsx
+++ src/pages/accommodation/Accommodation.jsx
@@ -3,6 +3,7 @@ import Navbar from '../../components/navBar/NavBar';
 import Footer from '../../components/footer/Footer';
 import Data from '../../data/Data.json';
 import { Navigate, useParams } from 'react-router-dom';
+import Error from '../pageError/Error';
 import'./accommodation.css'
 
 import Description from '../../components/description/Description';
@@ -12,7 +13,7 @@ function Accommodation() {
   /**Récupération de l'id de l'accommodation depuis les paramètres de l'url grace au useParams */
 
   const idAccommodation = useParams('id').id;
-
+   
  
    
   /** on parcourt les data en comparant l'id récupérer de url avec id qui se trouve dans les dats
@@ -22,9 +23,15 @@ function Accommodation() {
   const acommodationDetailSingle = Data.filter(
     (detail) => detail.id === idAccommodation
   );
-   
+  if (acommodationDetailSingle.length === 0) {
+    return (<Error/>);
+  }
+/**  */
+    if(idAccommodation  === undefined){
+return (<Error/>);
+  }
   /**récupération les données necessaire pour afficher la page d'apartement */
-  const pictures = acommodationDetailSingle[0].pictures
+  const pictures = acommodationDetailSingle[0].pictures;
   
   const description = acommodationDetailSingle[0].description;
   const equipments = acommodationDetailSingle[0].equipments;
@@ -37,14 +44,9 @@ function Accommodation() {
   const hosts = acommodationDetailSingle[0].host;
   const rates = acommodationDetailSingle[0].rating;
   /** vérification si le tableau accomoodationDetailSingle est vide donc on redirige l"utilisateur vers la page notFond */
-  if (acommodationDetailSingle.length === 0) {
-    return <Navigate to="/notfind" replace />;
-  }
-  /**
-   *  if(idAccommodation === undefined){
-return <Navigate to="/notfind" replace />;
-  }
-   */
+  console.log(acommodationDetailSingle);
+ 
+   
   return (
     <div className="container-accommoadtion">
       <Navbar />
