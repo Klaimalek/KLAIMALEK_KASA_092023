148c07860c0e99964b07c317a2dd9f88cbc94600
Malek <absimalekmootez@gmail.com>
2023-10-23 16:50:28 +0200

    modification du css de footer pour qu'il soit 100% de la page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Header from '../../components/header/Header'
-import ApartmentGrid from '../../components/apartments/ApartmentGrid.jsx'
-import Footer from '../../components/footer/Footer'
-import'./home.css'
+import React from 'react';
+import Header from '../../components/header/Header';
+import ApartmentGrid from '../../components/apartments/ApartmentGrid.jsx';
+import Footer from '../../components/footer/Footer';
+import './home.css';
 function Home() {
   return (
-    <div className='home'>
-      <Header/>
-      <ApartmentGrid/>
-      <Footer/>
-    </div>
-  )
+    <div className="home">
+      <Header />
+      <ApartmentGrid />
+      
+        <Footer/>
+      </div>
+    
+  );
 }
 
-export default Home
+export default Home;
